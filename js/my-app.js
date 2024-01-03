dd9264c4e01e6fc95eeff43e9b1432cd8f1c9fdc
khairulabdi154 <132919245+khairulabdi154@users.noreply.github.com>
2024-01-04 00:47:38 +0700

    Update my-app.js

--- js/my-app.js
+++ js/my-app.js
@@ -281,3 +281,22 @@ $$('.messagebar .link').on('click', function () {
 })           
 
 $locationProvider.html5Mode(true); //activate HTML5 Mode
+
+window.addEventListener('load', function(e)
+{
+  as = document.querySelectorAll('a[href^="#"]');
+  as.forEach(function(a)
+  {
+    a.addEventListener('click', function(e)
+    {
+      e.preventDefault();
+      
+      var hash = e.target.href.split('#')[1];
+      var targetEl = document.getElementById(hash);
+      
+      window.scrollTo(0, targetEl.offsetTop);
+      
+      console.log(targetEl.offsetTop)
+    });
+  });
+});
